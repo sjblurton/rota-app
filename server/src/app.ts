47f7827309cdb4455f1ea1ca19d0da1395687999
Simon Blurton <sblurton@hotmail.co.uk>
2026-03-21 12:42:22 +0000

    feat: add superadmin router to API endpoints

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,14 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 
 import { openApiDocument } from "./docs/openapi";
+import { superadminRouter } from "./modules/superadmin/routes/superadmin-router";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/superadmin", superadminRouter);
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
