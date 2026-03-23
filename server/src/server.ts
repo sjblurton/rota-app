45705a25c42f4e6f69c81779600ef2c36395b961
Simon Blurton <sblurton@hotmail.co.uk>
2026-03-23 18:58:38 +0000

    feat: enhance server logging in development mode using logger utility

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,18 @@
+import dotenv from "dotenv";
 import app from "./app";
+import { logger } from "./lib/logger";
+
+dotenv.config({ path: ".env.local" });
+dotenv.config();
 
 const PORT = process.env["PORT"] || 3000;
+const ENV = process.env["NODE_ENV"] || "development";
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  if (ENV === "development") {
+    logger.info(`Server running on port http://localhost:${PORT}`);
+    logger.info(
+      `API documentation available at http://localhost:${PORT}/api/docs`,
+    );
+  }
 });
