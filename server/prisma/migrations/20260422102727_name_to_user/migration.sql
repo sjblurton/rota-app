29fe8214af5054b08bc32bbfbc75a8c01fb4da00
Simon Blurton <sblurton@hotmail.co.uk>
2026-04-22 11:27:41 +0100

    feat: add preferred_contact_method to Invite model and update User model with name field

---
+++ server/prisma/migrations/20260422102727_name_to_user/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Invite" ALTER COLUMN "expires_at" SET DEFAULT CURRENT_TIMESTAMP + INTERVAL '7 days';
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "name" TEXT NOT NULL;
