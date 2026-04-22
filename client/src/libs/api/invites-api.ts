9069b4a5400c4f093e9696893a1a002b6ff190f7
Simon Blurton <sblurton@hotmail.co.uk>
2026-04-22 13:38:37 +0100

    style: format code in axios-instance and invites-api for consistency

--- client/src/libs/api/invites-api.ts
+++ client/src/libs/api/invites-api.ts
@@ -1,9 +1,13 @@
-import { api } from './axios-instance';
+import { api } from './axios-instance'
 
 export async function patchInvite(inviteId: string, token: string) {
-  return api.patch(`/invites/${inviteId}`, {}, {
-    headers: {
-      Authorization: `Bearer ${token}`,
+  return api.patch(
+    `/invites/${inviteId}`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     },
-  });
+  )
 }
