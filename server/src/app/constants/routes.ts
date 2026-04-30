015052db8545b037388c56a9936574868ca73e87
Simon Blurton <sblurton@hotmail.co.uk>
2026-04-30 10:13:14 +0100

    feat: add routes for admin and superadmin along with Swagger documentation path

---
+++ server/src/app/constants/routes.ts
@@ -0,0 +1,17 @@
+import { type Router } from "express";
+
+import { adminRouter } from "../../routers/admin/admin.router";
+import { ADMIN_BASE_PATH } from "../../routers/admin/constants/admin.routes";
+import { SUPERADMIN_BASE_ROUTE } from "../../routers/superadmin/constants/superadmin.routes";
+import { superadminRouter } from "../../routers/superadmin/superadmin.router";
+
+export const ROOT_ROUTES: { path: string; router: Router }[] = [
+  {
+    path: SUPERADMIN_BASE_ROUTE,
+    router: superadminRouter,
+  },
+  {
+    path: ADMIN_BASE_PATH,
+    router: adminRouter,
+  },
+];
---
+++ server/src/docs/constants/docs.routes.ts
@@ -0,0 +1,3 @@
+import { PATHS } from "../../constants/paths";
+
+export const SWAGGER_DOCS_BASE_PATH = `${PATHS.apiBaseV1}${PATHS.docs}`;
