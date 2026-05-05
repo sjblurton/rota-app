0643f68a0a91844d7a4b70d6bee8ddcf1e25783a
Simon Blurton <sblurton@hotmail.co.uk>
2026-05-05 15:01:04 +0100

    feat: add update staff input and body types to staff schema

--- server/src/@types/staff.ts
+++ server/src/@types/staff.ts
@@ -4,9 +4,13 @@ import type {
   createStaffBodySchema,
   createStaffSchema,
   staffSchema,
+  updateStaffBodySchema,
+  updateStaffSchema,
 } from '../libs/schemas/entities/staff'
 
 export type CreateStaffInput = z.infer<typeof createStaffSchema>
 export type CreateStaffBody = z.infer<typeof createStaffBodySchema>
+export type UpdateStaffInput = z.infer<typeof updateStaffSchema>
+export type UpdateStaffBody = z.infer<typeof updateStaffBodySchema>
 
 export type Staff = z.infer<typeof staffSchema>
