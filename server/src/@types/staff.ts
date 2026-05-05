3120b33daa10dc623e50253bd2b15aafbf11a73e
Simon Blurton <sblurton@hotmail.co.uk>
2026-05-05 14:51:45 +0100

    feat: update organisation and staff types to include pagination and staff schemas

--- server/src/@types/staff.ts
+++ server/src/@types/staff.ts
@@ -1,5 +1,12 @@
 import type z from 'zod'
 
-import type { createStaffSchema } from '../libs/schemas/entities/staff'
+import type {
+  createStaffBodySchema,
+  createStaffSchema,
+  staffSchema,
+} from '../libs/schemas/entities/staff'
 
 export type CreateStaffInput = z.infer<typeof createStaffSchema>
+export type CreateStaffBody = z.infer<typeof createStaffBodySchema>
+
+export type Staff = z.infer<typeof staffSchema>
