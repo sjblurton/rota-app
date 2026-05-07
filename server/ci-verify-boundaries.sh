16921779c124c81622bef04a7081b29411b9b75c
Simon Blurton <sblurton@hotmail.co.uk>
2026-05-07 12:28:07 +0100

    chore: comment out debugging commands in ci-verify-boundaries.sh for clarity

--- server/ci-verify-boundaries.sh
+++ server/ci-verify-boundaries.sh
@@ -2,14 +2,17 @@
 set -e
 
 # Print working directory and list boundaries config files
-pwd
-ls -l eslint/boundaries/
+# pwd
+# ls -l eslint/boundaries/
 
-# Print the contents of elements.mjs
+# # Print break
+# printf "\n---\n\n"
+# # Print the contents of elements.mjs
 cat eslint/boundaries/elements.mjs
-
-# Print the first 20 lines of eslint.config.mjs
-head -20 eslint.config.mjs
-
-# Print the boundariesElements variable definition
-awk '/export const boundariesElements = \[/, /\]/ { print }' eslint/boundaries/elements.mjs
+# printf "\n---\n\n"
+# # Print the first 20 lines of eslint.config.mjs
+# head -20 eslint.config.mjs
+# printf "\n---\n\n"
+# # Print the boundariesElements variable definition
+# awk '/export const boundariesElements = \[/, /\]/ { print }' eslint/boundaries/elements.mjs
+# printf "\n---\n\n"
